10ec3861072a258798c3a0a05462aec5b6a6bbcf
shavtvalishvili <gshavtvalishvili@gmail.com>
2021-06-22 21:18:13 +0400

    feat: update carousel component and add slide data array generation and detail toggle functionality

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,24 +1,65 @@
 import React from "react"
-import { useState } from 'react'
-import "./Carousel.css";
-import SlidesContainer from "../SlidesContainer/SlidesContainer"
+import { useState, useRef, useEffect } from 'react'
+import "./Carousel.css"
+import MainSlidesContainer from "../MainSlidesContainer/MainSlidesContainer"
+import PreviewSlidesContainer from "../PreviewSlidesContainer/PreviewSlidesContainer"
 
-const Carousel = ({content}) => {
-  let id = 0
-  const [slides, updateSlides] = useState(content.map(slideHtml => (
+const Carousel = ({
+  mainSlideElements,
+  previewSlideElements,
+  width,
+  height,
+  previewSlidesHeightPct,
+  slideBackgroundColor,
+  transitions
+}) => {
+  const range = [...Array(mainSlideElements.length).keys()];
+  const mainSlidesData = range.map(id => (
     {
-      className: id == 0 ? "currentSlide" : id == content.length - 1 ? "previousSlide" : id == 1 ? "nextSlide" : "",
-      id: id++,
-      html: slideHtml
+      id: id,
+      reactElement: mainSlideElements[id],
+      reference: useRef()
     }
-  )));
+  ));
 
-
+  const [animationFinished, setAnimationFinished] = useState(true);
+  const [detailsVisibility, setDetailsVisibility] = useState(false);
+  const [detailsFade, setDetailsFade] = useState(true);
+  const toggleDetails = () => {
+    if (animationFinished && !detailsVisibility) {
+      setAnimationFinished(false);
+      setDetailsVisibility(true);
+      setTimeout(() => setDetailsFade(false), 1);
+      setTimeout(() => setAnimationFinished(true), 601);
+    } else if (animationFinished && detailsVisibility) {
+      setAnimationFinished(false);
+      setDetailsFade(true);
+      setTimeout(() => setDetailsVisibility(false), 600);
+      setTimeout(() => setAnimationFinished(true), 600);
+    }
+  }
 
   return (
-    <div className="Carousel">
-      <SlidesContainer slides={slides} />
-      {/* <SlidesPreviewContainer /> */}
+    <div
+      className="Carousel"
+      style={{maxWidth: width, maxHeight: height}}
+    >
+      <MainSlidesContainer
+        slidesData={mainSlidesData}
+        toggleDetails={toggleDetails}
+        transitions={transitions}
+        navigationVisible={detailsVisibility}
+        navigationFade={detailsFade}
+        slideBackgroundColor={slideBackgroundColor}
+      />
+      <PreviewSlidesContainer
+        previewSlideElements={previewSlideElements}
+        visible={detailsVisibility}
+        fade={detailsFade}
+        // currentSlideIdx={mainSlides.reduce((accumulator, currentValue) =>
+        //   (currentValue.className === "currentSlide" ? currentValue.id : accumulator), 0)}
+        heightPct={previewSlidesHeightPct}
+      />
     </div>
   );
 }
