082de1f61d6a8e59cd6b1bb736406fa80fa7f72b
shavtvalishvili <gshavtvalishvili@gmail.com>
2021-06-22 21:12:08 +0400

    feat: introduce feature rich dynaimc slide container

--- src/components/SlideContainer/SlideContainer.js
+++ src/components/SlideContainer/SlideContainer.js
@@ -1,10 +1,30 @@
 import React from "react"
 import "./SlideContainer.css"
 
-const SlideContainer = ({className, html}) => {
+const SlideContainer = ({
+  slideData,
+  state,
+  transitions,
+  handleInteractionStart,
+  backgroundColor
+}) => {
+  const style = {backgroundColor: backgroundColor};
+  for (let i = 0; i < transitions.length; i++) {
+    const tr = transitions[i];
+    style[tr.property] = tr.surroundingStrings.join(tr[state]);
+  }
+  style.zIndex = state === "current" ? 1 : 0;
+  
   return (
-    <div className={"SlideContainer " + className}>
-      {html}
+    <div
+      value={slideData.id}
+      className={"SlideContainer"}
+      style={style}
+      onMouseDown={state === "current" ? handleInteractionStart : null}
+      onTouchStart={state === "current" ? handleInteractionStart : null}
+      ref={slideData.reference}
+    >
+      {slideData.reactElement}
     </div>
   )
 }
